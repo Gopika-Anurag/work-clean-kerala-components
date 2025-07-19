aea0dc96ea84c17dbbfcf363c90d6a63ccf4ff34
GopikaAnurag <gopikarahul2016@gmail.com>
2025-07-19 17:11:18 +0530

    19.07.2025 first commit adding pie daigram on activities at glance

--- src/data/Carouseldata.jsx
+++ src/data/Carouseldata.jsx
@@ -23,7 +23,7 @@ export const activitiesAtGlance = [
           {
         year: "2023–2024",
         value: 30,
-        color: "#10B981"
+        color: "#EF444"
       },
       {
         year: "2024–2025",
